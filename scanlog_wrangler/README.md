69b9e4293283bb0b1109cb5414c4f0b51927aad4
Smitty <me@smitop.com>
2022-02-03 16:35:36 -0500

    start on dolog script

--- scanlog_wrangler/README.md
+++ scanlog_wrangler/README.md
@@ -3,4 +3,10 @@
 
 This manages invokations of the `scanlog` binary of [certificate-transparency-go](https://github.com/google/certificate-transparency-go) to maintain a complete history of certificates across multiple logs. Unlike `scanlog` itself, `scanlog_wrangler` can resume if it is stopped without needing to fetch all certificates from scratch again. Once all certificates are obtained for a log, `scanlog_wrangler` periodically fetches any new certificates that may have been added to the log.
 
-If this is used to manage a significant number of certificates, you may wish to store them on a compressed file system (such as ZFS).
+If this is used to manage a significant number of certificates, you may wish to store them on a compressed file system (such as ZFS) with deduplication support.
+
+## Usage
+Arguments: `scanlog` path, log base URL, certificate base path, log name
+```sh
+node dolog.js ~/certificate-transparency-go/scanlog "https://ct.cloudflare.com/logs/nimbus2023" /tmp/certs nimbus2023
+```
---
+++ scanlog_wrangler/dolog.js
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: Apache-2.0
+const { spawnSync } = require("child_process");
+const fs = require("fs");
+const https = require("https");
+const assert = require("assert").strict;
+
+const STRIDE = 20000;
+
+(async () => {
+    // setup
+
+    const scanlogPath = process.argv[2];
+    const logUrl = process.argv[3];
+    const certsPath = process.argv[4];
+    const logId = process.argv[5];
+    if (!scanlogPath || !logUrl || !certsPath || !logId) {
+        throw new Error("not all inputs specified");
+    }
+
+    const logPath = `${certsPath}/${logId}`;
+    const logMetaPath = `${certsPath}/${logId}.json`;
+    if (!fs.existsSync(logPath)) {
+        fs.mkdirSync(logPath, { recursive: true });
+    }
+
+    let meta;
+    if (fs.existsSync(logMetaPath)) {
+        meta = JSON.parse(fs.readFileSync(logMetaPath, "utf-8"));
+    } else {
+        meta = { idx: 0 };
+        metaChanged();
+    }
+    function metaChanged() {
+        fs.writeFileSync(logMetaPath, JSON.stringify(meta), "utf-8");
+    }
+
+    const sthUri = logUrl + "/ct/v1/get-sth";
+    let sth;
+    await new Promise(resolve => https.get(sthUri, res => {
+        let body = "";
+        res.on("data", chunk => body += chunk);
+        res.on("end", () => {
+            const data = JSON.parse(body);
+            sth = data;
+            resolve();
+        });
+    }));
+    if (meta.last_sth) {
+        if (sth.tree_size > meta.last_sth.tree_size) {
+            console.warn("CT log truncated", meta.last_sth, sth);
+            throw new Error("CT log is broken");
+        }
+        if (sth.tree_size === meta.idx) {
+            console.log("No certs appended since last check");
+            return;
+        }
+    }
+    meta.last_sth = sth;
+
+    const certCount = fs.readdirSync(logPath).length;
+    assert.equal(certCount, meta.idx);
+
+    // fetch certs
+
+    console.log("Initialized, starting fetching");
+    while (meta.idx < sth.tree_size) {
+        const start = Date.now();
+        spawnSync(
+            scanlogPath,
+            [
+                "-log_uri",
+                logUrl,
+                
+                "-dump_dir",
+                logPath,
+
+                "-start_index", // inclusive
+                meta.idx.toString(), // meta.idx points to next certificate to fetch
+
+                "-end_index", // exclusive
+                (meta.idx + STRIDE).toString(),
+
+                "-batch_size",
+                "100",
+
+                "-parallel_fetch",
+                "4",
+
+                "-dump_full_chain=false",
+            ],
+        );
+        const end = Date.now();
+        const duration = end - start;
+        const certCount = fs.readdirSync(logPath).length;
+        assert.equal(certCount, meta.idx + STRIDE);    
+        meta.idx += STRIDE;
+        console.log(`Fetched ${STRIDE} certs in ${(duration / 1000).toFixed(1)}s, ${(duration / STRIDE).toFixed(1)}ms per cert`);
+    }
+    console.log("Caught up to CT log");
+})();

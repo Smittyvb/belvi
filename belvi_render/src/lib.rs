0d1ab9214fed325f3416609dd2fb79b1e46f1b38
Smitty <me@smitop.com>
2022-02-10 19:53:52 -0500

    pass through rendering on wrapper cert

--- belvi_render/src/lib.rs
+++ belvi_render/src/lib.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 //! Rendering of various CT-related things.
 
-use x509_certificate::certificate::X509Certificate;
+use x509_certificate::{certificate::X509Certificate, rfc5280::Certificate};
 
 mod oid;
 
@@ -10,6 +10,13 @@ pub trait Render {
 }
 
 impl Render for X509Certificate {
+    fn render(&self) -> String {
+        let cert: &Certificate = self.as_ref();
+        cert.render()
+    }
+}
+
+impl Render for Certificate {
     fn render(&self) -> String {
         format!("{:#?}", self)
     }

734fb8ae93244665558baa91b037feb2a24e8873
Smitty <me@smitop.com>
2022-02-12 10:01:16 -0500

    support null in ber

--- belvi_render/src/ber.rs
+++ belvi_render/src/ber.rs
@@ -7,6 +7,9 @@ use super::{html_escape::HtmlEscapable, Render};
 pub fn render_ber(bytes: bytes::Bytes) -> String {
     let orig_bytes = bytes.clone();
     if let Ok(text) = Constructed::decode(bytes, Mode::Der, |cons| {
+        if let Ok(()) = cons.take_null() {
+            return Ok(r#"<span class="bvcert-null">NULL</span>"#.to_string());
+        }
         PrintableString::take_from(cons).map(|str| {
             String::from_utf8(str.into_bytes().to_vec())
                 .unwrap()

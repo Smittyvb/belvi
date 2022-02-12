2e0d5e97e95cdfcc97c8d18c596cec5a0b4ec39b
Smitty <me@smitop.com>
2022-02-12 10:22:26 -0500

    improve ber handling

--- belvi_render/src/ber.rs
+++ belvi_render/src/ber.rs
@@ -4,29 +4,44 @@ use bcder::{decode::Constructed, Mode};
 
 use super::{html_escape::HtmlEscapable, Render};
 
-pub fn render_ber(bytes: bytes::Bytes) -> String {
-    let orig_bytes = bytes.clone();
-    if let Ok(text) = Constructed::decode(bytes, Mode::Der, |cons| {
-        if let Ok(()) = cons.take_null() {
-            return Ok(r#"<span class="bvcert-null">NULL</span>"#.to_string());
-        }
-        macro_rules! string_type {
-            ($str:ident) => {
-                if let Ok(str) = bcder::$str::take_from(cons) {
-                    return Ok(String::from_utf8(str.into_bytes().to_vec())
-                        .unwrap()
-                        .html_escape());
-                }
-            };
-        }
-        string_type!(Ia5String);
-        string_type!(NumericString);
-        string_type!(PrintableString);
-        string_type!(Utf8String);
+fn take_cons(cons: &mut Constructed<bytes::Bytes>) -> Result<String, bcder::decode::Error> {
+    if let Ok(()) = cons.take_null() {
+        return Ok(r#"<span class="bvcert-null">NULL</span>"#.to_string());
+    }
+
+    macro_rules! string_type {
+        ($str:ident) => {
+            if let Ok(str) = bcder::$str::take_from(cons) {
+                return Ok(String::from_utf8(str.into_bytes().to_vec())
+                    .unwrap()
+                    .html_escape());
+            }
+        };
+    }
+    string_type!(Ia5String);
+    string_type!(NumericString);
+    string_type!(PrintableString);
+    string_type!(Utf8String);
+
+    if let Ok(str) = bcder::OctetString::take_from(cons) {
+        return Ok(str.into_bytes().render());
+    }
+
+    if let Ok(s) = cons.take_sequence(|x| {
+        dbg!(x);
         Err(bcder::decode::Error::Malformed)
     }) {
+        return Ok(s);
+    }
+
+    Err(bcder::decode::Error::Malformed)
+}
+
+pub fn render_ber(bytes: bytes::Bytes) -> String {
+    let orig_bytes = bytes.clone();
+    if let Ok(text) = Constructed::decode(bytes, Mode::Der, take_cons) {
         text
     } else {
-        orig_bytes.render()
+        format!("Unparsed DER: {}", orig_bytes.render())
     }
 }

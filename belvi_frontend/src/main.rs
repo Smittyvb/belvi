93136c215a36134138422e1da6cebec3477eec6b
Smitty <me@smitop.com>
2022-02-21 14:20:23 -0500

    render cert count

--- belvi_frontend/src/main.rs
+++ belvi_frontend/src/main.rs
@@ -1,26 +1,34 @@
 // SPDX-License-Identifier: Apache-2.0
 
 use axum::{routing::get, Router};
-use log::info;
 use rusqlite::{Connection, OpenFlags};
 use std::{env, path::PathBuf};
 
-#[tokio::main]
+// TODO: use tokio::task_local instead?
+thread_local! {
+    static DB_CONN: Connection = {
+        let mut args = env::args_os();
+        let data_path: PathBuf = args.nth(1).unwrap().into();
+        let db_path = data_path.clone().join("data.db");
+        // OPEN_CREATE isn't passed, so we don't create the DB if it doesn't exist
+        Connection::open_with_flags(db_path, OpenFlags::SQLITE_OPEN_READ_ONLY).unwrap()
+    };
+}
+
+async fn get_root() -> String {
+    DB_CONN.with(|db| {
+        let count: usize = db
+            .query_row("SELECT count(domain) FROM domains", [], |row| row.get(0))
+            .unwrap();
+        format!("Currently {} certs", count)
+    })
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
     env_logger::init();
 
-    let mut args = env::args_os();
-    let data_path: PathBuf = args.nth(1).unwrap().into();
-    let db_path = data_path.clone().join("data.db");
-    // OPEN_CREATE isn't passed, so we don't create the DB if it doesn't exist
-    let db_conn = Connection::open_with_flags(db_path, OpenFlags::SQLITE_OPEN_READ_ONLY).unwrap();
-
-    let count: usize = db_conn
-        .query_row("SELECT count(domain) FROM domains", [], |row| row.get(0))
-        .unwrap();
-    info!("Currently {} certs", count);
-
-    let app = Router::new().route("/", get(|| async { "Hello world" }));
+    let app = Router::new().route("/", get(get_root));
 
     axum::Server::bind(&"0.0.0.0:47371".parse().unwrap())
         .serve(app.into_make_service())

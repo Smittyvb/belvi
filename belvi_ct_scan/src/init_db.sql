e41492de9b5db9b29cd4bc9eaee94ab2c46025fb
Smitty <me@smitop.com>
2022-02-21 14:01:33 -0500

    multiple certs for same domain

--- belvi_ct_scan/src/init_db.sql
+++ belvi_ct_scan/src/init_db.sql
@@ -1,28 +1,28 @@
 -- SPDX-License-Identifier: Apache-2.0
 -- Run every time the SQLite database is loaded.
 
--- INITALIZE TABLES --
+BEGIN;
 
+-- INITALIZE TABLES --
 CREATE TABLE IF NOT EXISTS meta(
     k TEXT PRIMARY KEY,
     v TEXT
 ); -- WITH ROWID
-
 -- if schema is changed in future, an actual system for migrating the DB will be implemented
 -- for now, just assume we are always on the initial version
 INSERT OR REPLACE into meta (k, v) values ("migration", "1.0.0");
-
 CREATE TABLE IF NOT EXISTS certs (
     leaf_hash BLOB PRIMARY KEY NOT NULL,
     extra_hash BLOB NOT NULL,
     ts NUMBER NOT NULL
 ) WITHOUT ROWID;
-
 CREATE TABLE IF NOT EXISTS domains (
-    -- TODO: multiple certs for same domain
-    domain TEXT PRIMARY KEY NOT NULL,
+    domain TEXT NOT NULL,
     leaf_hash BLOB NOT NULL
-) WITHOUT ROWID;
+); -- WITH ROWID
+
+-- CREATE INDICIES --
+CREATE INDEX IF NOT EXISTS idx_domains_domain1 ON domains (domain);
 
 -- CONFIGURE SQLITE --
 PRAGMA journal_mode = WAL;
@@ -31,4 +31,5 @@ PRAGMA user_version = 1;
 
 -- OPTIMIZE DB --
 PRAGMA optimize;
-VACUUM;
+
+COMMIT;

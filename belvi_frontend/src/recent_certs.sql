3d7b1a370ac5099415164f7b19f96c4ac1ce28cf
Smitty <me@smitop.com>
2022-02-22 20:35:53 -0500

    query for getting recent certs

---
+++ belvi_frontend/src/recent_certs.sql
@@ -0,0 +1,6 @@
+SELECT log_entries.leaf_hash, log_entries.log_id, log_entries.ts, domains.domain, certs.extra_hash, certs.not_before, certs.not_after
+FROM log_entries
+LEFT JOIN domains ON log_entries.leaf_hash = domains.leaf_hash
+LEFT JOIN certs ON log_entries.leaf_hash = certs.leaf_hash
+ORDER BY log_entries.ts
+LIMIT 10

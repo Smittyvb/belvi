cd6c179db925b8002f88e49f00519e32edb2a76b
Smitty <me@smitop.com>
2022-02-23 15:27:36 -0500

    use reverse-chronological order

--- belvi_frontend/src/recent_certs.sql
+++ belvi_frontend/src/recent_certs.sql
@@ -3,5 +3,5 @@ SELECT log_entries.leaf_hash, log_entries.log_id, log_entries.ts, domains.domain
 FROM log_entries
 LEFT JOIN domains ON log_entries.leaf_hash = domains.leaf_hash
 LEFT JOIN certs ON log_entries.leaf_hash = certs.leaf_hash
-ORDER BY log_entries.ts
+ORDER BY log_entries.ts DESC
 LIMIT 100

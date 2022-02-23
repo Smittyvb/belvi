29e8532d19c61199ad173d7d587d3126ee09cb74
Smitty <me@smitop.com>
2022-02-23 13:55:06 -0500

    license

--- belvi_frontend/src/recent_certs.sql
+++ belvi_frontend/src/recent_certs.sql
@@ -1,3 +1,4 @@
+-- SPDX-License-Identifier: Apache-2.0
 SELECT log_entries.leaf_hash, log_entries.log_id, log_entries.ts, domains.domain, certs.extra_hash, certs.not_before, certs.not_after
 FROM log_entries
 LEFT JOIN domains ON log_entries.leaf_hash = domains.leaf_hash

c376d0196306ae164a691bf4ef5d8ce808af0560
Smitty <me@smitop.com>
2022-07-04 19:03:21 -0400

    add some regex complexity limits

--- belvi_frontend/src/exts.rs
+++ belvi_frontend/src/exts.rs
@@ -4,6 +4,14 @@ use regex::bytes::{Regex, RegexBuilder};
 use rusqlite::{functions::FunctionFlags, Connection};
 use std::sync::Arc;
 
+fn configure_regex(b: &mut RegexBuilder) {
+    b
+        // certificates usually (but not always) write names in lowercase
+        .case_insensitive(true)
+        .size_limit(10000)
+        .nest_limit(18);
+}
+
 pub fn register(db: &mut Connection) {
     // https://docs.rs/rusqlite/latest/rusqlite/functions/index.html
     db.create_scalar_function(
@@ -15,10 +23,9 @@ pub fn register(db: &mut Connection) {
             let regex: Arc<Regex> = ctx.get_or_create_aux(
                 0,
                 |vr| -> Result<_, Box<dyn std::error::Error + Send + Sync + 'static>> {
-                    Ok(RegexBuilder::new(vr.as_str()?)
-                        // certificates usually (but not always) write names in lowercase
-                        .case_insensitive(true)
-                        .build()?)
+                    let mut builder = RegexBuilder::new(vr.as_str()?);
+                    configure_regex(&mut builder);
+                    Ok(builder.build()?)
                 },
             )?;
             Ok(match ctx.get_raw(1).as_str() {
@@ -60,5 +67,20 @@ mod test {
         assert_match(&mut db, "regex('c', 'bcd')", true);
         assert_match(&mut db, "regex('^c', 'bcd')", false);
         assert_match(&mut db, "regex('^e', 'bcd')", false);
+    }
+
+    /// Ensures that the regex complexity limits still allow certain regexes.
+    #[test]
+    pub fn complexity() {
+        fn test_regex(r: &'static str, valid: bool) {
+            let mut builder = RegexBuilder::new(r);
+            configure_regex(&mut builder);
+            assert_eq!(builder.build().is_ok(), valid);
+        }
+        // email regex, https://stackoverflow.com/a/201378/10113238
+        test_regex(
+            r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#,
+            true,
+        );
     }
 }

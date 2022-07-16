5feedf9bc3c78e099b4fcf8a7c6f98f6cd581a35
Smitty <me@smitop.com>
2022-07-16 14:59:13 -0400

    add CLI tool to search certs for debugging/perf

---
+++ belvi_frontend/src/bin/search.rs
@@ -0,0 +1,29 @@
+use std::time::Instant;
+
+// SPDX-License-Identifier: Apache-2.0
+use belvi_frontend::search;
+
+fn main() {
+    env_logger::init();
+
+    let db = belvi_frontend::db::connect();
+    let limit = 50;
+    let query = search::Query {
+        domain: std::env::args_os().nth(2).map(|s| s.into_string().unwrap()),
+        limit: Some(limit),
+    };
+
+    let start = Instant::now();
+    let (certs, count) = match query.search_sync(&db, limit) {
+        Ok(v) => v,
+        Err(res) => panic!("failed: {:?}", res.body()),
+    };
+    let end = Instant::now();
+    let duration = end - start;
+
+    let len = certs.len();
+    for cert in certs {
+        println!("{:?}", cert);
+    }
+    println!("Found {}/{:?} certs in {:?}", len, count, duration);
+}

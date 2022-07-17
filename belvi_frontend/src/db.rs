ffe57a8559c9df3c71185839068cfbdb79ad816b
Smitty <me@smitop.com>
2022-07-17 10:56:09 -0400

    go back to readonly in frontend
    perhaps this can help with busy issues?

--- belvi_frontend/src/db.rs
+++ belvi_frontend/src/db.rs
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-use rusqlite::Connection;
+use rusqlite::{Connection, OpenFlags};
 use std::{env, path::PathBuf};
 
 // SPDX-License-Identifier: Apache-2.0
@@ -11,10 +11,7 @@ fn get_data_path() -> PathBuf {
 pub fn connect() -> Connection {
     let db_path = get_data_path().join("data.db");
     // OPEN_CREATE isn't passed, so we don't create the DB if it doesn't exist
-    let mut db = Connection::open(db_path).unwrap();
-    // this can write to the database so we can't open it as readonly
-    db.execute_batch(include_str!("../../shared_sql/init_db.sql"))
-        .unwrap();
+    let mut db = Connection::open_with_flags(db_path, OpenFlags::SQLITE_OPEN_READ_ONLY).unwrap();
     crate::exts::register(&mut db);
     db
 }

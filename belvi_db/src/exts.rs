2779abb31ae0fb1feddc4636fbb896b23af9498a
Smitty <me@smitop.com>
2022-07-26 12:41:11 -0400

    increase regex size limit

--- belvi_db/src/exts.rs
+++ belvi_db/src/exts.rs
@@ -8,7 +8,7 @@ fn configure_regex(b: &mut RegexBuilder) {
     b
         // certificates usually (but not always) write names in lowercase
         .case_insensitive(true)
-        .size_limit(10000)
+        .size_limit(27500)
         .nest_limit(18);
 }
 
@@ -111,6 +111,7 @@ mod test {
             r#"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"#,
             true,
         );
+        test_regex(r"^d(i|l|1)[a-z0-9]{3,6}ds?(-[a-z0-9-]{0,10})?(\.[a-z]{1,4}){1,3}$", true);
     }
 
     #[test]
